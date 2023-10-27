81a3d2a4af1d9a9e441e357aa0ff12dbc896eb64
WasathTheekshana <wasath.vt@gmail.com>
2023-10-27 19:32:36 +0530

    feat: social media linkes

---
+++ src/constants/social-handles.ts
@@ -0,0 +1,8 @@
+export const facebook = "https://www.facebook.com/foss.nsbm";
+export const twitter = "https://twitter.com/fossnsbm";
+export const instagram = "https://www.instagram.com/fossnsbm";
+export const linkedIn = "https://www.linkedin.com/company/fossnsbm/";
+export const youTube = "https://youtube.com/@fosscommunitynsbm8708";
+export const tikTok = "https://www.tiktok.com/@fossnsbm";
+export const medium = "https://medium.com/foss-nsbm";
+export const github = "https://github.com/fossnsbm";
