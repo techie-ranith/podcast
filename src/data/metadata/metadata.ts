f8dcd8f272cfacf855d074821111c981a3b81957
WasathTheekshana <wasath.vt@gmail.com>
2023-10-27 19:33:30 +0530

    feat: dynemic metadata

---
+++ src/data/metadata/metadata.ts
@@ -0,0 +1,14 @@
+export const main = {
+    title: "Dialectic",
+    description: "Dialectic by FOSS Community of NSBM",
+    keywords: "FOSS, Free & Open Source, Podcast, Tech Podcast" 
+}
+
+export const episodes = {
+    title: "All Episodes",
+}
+
+export const favorites = {
+    title: "My Favorites",
+}
+
