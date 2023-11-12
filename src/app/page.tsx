aa94168a94402dd6a0d2a3a8c704f38e24fa3371
yohan manohara <warushayoham80@gmail.com>
2023-11-12 09:57:12 +0530

    feat(issue-40): complete episode card but not made the responsive changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { Savedbar } from "@/components/common/layout/";
+import { Episodecard } from "@/components/common/layout/";
 type Props = {};
 
 const Home = (props: Props) => {
-  return <></>;
+  return (
+    <>
+      <main className="bg-gray-900 h-screen">
+        <Episodecard />
+      </main>
+    </>
+  );
 };
 
 export default Home;
