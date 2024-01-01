647039c53fe4c37019518524a984646a35ddc1db
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-01-01 14:57:39 +0530

    feat: foss section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "@/components/landing/Hero";
 import Popular from "@/components/popular_ep/Popular";
 import Logos from "@/components/landing/logos";
+import FOSS_Section from "@/components/landing/foss";
 
 type Props = {};
 
@@ -9,6 +10,7 @@ const Home = (props: Props) => {
   return (
     <div>
       <Hero />
+      <FOSS_Section />
       <Logos />
     </div>
   );
