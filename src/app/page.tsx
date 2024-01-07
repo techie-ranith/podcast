dc697e199cb7d6ee9e9027afe5dd1f3ab839f776
techie-ranith <ranith2k3@gmail.com>
2024-01-07 19:41:42 +0530

    feat: path changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "@/components/landing/Hero";
-import Popular_ep from "@/components/Popular_episodes/Popular_ep";
-import Popular_ep2 from "@/components/Popular_episodes/Popular_ep2";
+import Popular_ep from "@/components/popular_episodes/popular_ep";
+import Popular_ep2 from "@/components/popular_episodes/Popular_ep2"; // Fix: Correct the casing of the file path
 
 type Props = {};
 
--- src/components/popular_episodes/index.ts
+++ src/components/popular_episodes/index.ts
@@ -1,2 +1,2 @@
-import { default as Popular_ep } from "./Popular_ep";
+import { default as Popular_ep } from "./popular_ep";
 import { default as Popular_ep2 } from "./Popular_ep2";
