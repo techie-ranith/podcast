68d35293f5948f60e3dfab39c14f573fb144c592
techie-ranith <ranith2k3@gmail.com>
2024-01-15 09:23:26 +0530

    feat: updating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
+import { Episodecard, Savedbar } from '@/components/common/layout/'
 
-import { Savedbar } from "@/components/common/layout/";
+import React from 'react'
+import Hero from '@/components/landing/Hero'
+import Popular_ep from '@/components/popular_episodes/popular_ep'
+import Popular_ep2 from '@/components/popular_episodes/Popular_ep2'
+import Latest from '@/components/latest_episode/latest'
+import FOSS_Section from '@/components/landing/foss'
+import Logos from '@/components/landing/logos'
 
-import React from "react";
-import Hero from "@/components/landing/Hero";
-import Popular_ep from "@/components/popular_episodes/popular_ep";
-import Latest from "@/components/latest_episode/latest";
-import Popular_ep2 from "@/components/popular_episodes/Popular_ep2"; // Fix: Correct the casing of the file path
-import Popular from "@/components/popular_ep/Popular";
-import Logos from "@/components/landing/logos";
-import FOSS_Section from "@/components/landing/foss";
-
-
-type Props = {};
+type Props = {}
 
 const Home = (props: Props) => {
   return (
     <>
-      <main className="bg-gray-900 h-screen">
-        <Episodecard />
-      </main>
+      <Hero></Hero>
+      <Popular_ep2></Popular_ep2>
+      <Popular_ep></Popular_ep>
+      <Latest></Latest>
+      <Episodecard></Episodecard>
+      <FOSS_Section></FOSS_Section>
+      <Logos></Logos>
     </>
+  )
+}
 
-  );
-};
-
-export default Home;
+export default Home
