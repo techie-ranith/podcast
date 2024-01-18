0defea8c9c4f0ddfca857a4d4d75d4ca7c66b015
yohan manohara <warushayohan80@gmail.com>
2024-01-18 14:13:38 +0530

    feat(all-episode-page): complete the page but no navbar and footer issue:37

--- src/app/(client)/(pages)/episodes/page.tsx
+++ src/app/(client)/(pages)/episodes/page.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import Containerf from '@/components/common/containerf'
+import Searchq from '@/components/common/layout/search'
+import Episodecard from '@/components/common/layout/Episode_card'
+import { Button } from '@mui/material'
+import { episodes } from '@/data/metadata'
+import { Savedbar } from '@/components/common'
 
 type Props = {}
 
-const Episodes = (props: Props) => {
+const Allepisode = (props: Props) => {
   return (
-    <div>Episodes</div>
+    <div>
+      <Savedbar />
+      <Containerf>
+        <div className="w-full pb-36">
+          <div className=" text-[64px] float-left ">All Episode</div>
+
+          <div className=" float-right p-9 justify-center">
+            <Searchq />
+          </div>
+        </div>
+      </Containerf>
+
+      <Containerf>
+        <Episodecard />
+        <Episodecard />
+        <Episodecard />
+        <Episodecard />
+      </Containerf>
+
+      <Containerf>
+        <div className="w-full flex justify-center items-center  pt-24 pb-20">
+          <Button variant="outlined">View More</Button>
+        </div>
+      </Containerf>
+    </div>
   )
 }
 
-export default Episodes
+export default Allepisode
