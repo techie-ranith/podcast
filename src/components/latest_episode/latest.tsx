898d4d4307246087a1ebcb7363d0e3135283a248
yohan manohara <warushayohan80@gmail.com>
2024-01-24 13:14:36 +0530

    #

--- src/components/latest_episode/latest.tsx
+++ src/components/latest_episode/latest.tsx
@@ -8,40 +8,47 @@ import { FaRegCirclePlay } from 'react-icons/fa6'
 
 const latest = () => {
   return (
-    <div className=" h-max grid grid-col-1 md:grid-cols-2 mt-20 p-8  ">
-      <div>
-        <Image alt={'latest image'} src={latest_img} width={450} height={500} />
-      </div>
-      <div className="felx flex-col justify-between">
-        <div className="flex items-center justify-left gap-5 bg-gray-50 rounded-full pr-4 w-80 ">
+    <Containerf>
+      <div className=" h-max grid grid-col-1 md:grid-cols-2 mt-20 p-8  ">
+        <div>
+          <Image
+            alt={'latest image'}
+            src={latest_img}
+            width={450}
+            height={500}
+          />
+        </div>
+        <div className="felx flex-col justify-between">
+          <div className="flex items-center justify-left gap-5 bg-gray-50 rounded-full pr-4 w-80 ">
+            <div>
+              <Button variant={'blue'} size={'rounded'}>
+                Episode 1
+              </Button>
+            </div>
+            <div>Speaker Name | 33 mins</div>
+          </div>
+          <br />
+          <h1 className="text-5xl md:text-6xl font-medium tracking-wide">
+            Start career path as
+            <br /> a Product Designer <br />
+            in Startup
+          </h1>
+          <br />
+          <h1 className="text-4l text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et
+            est leo. Nulla fermentum ultrices ullamcorper. Nam eleifend mi sed
+            sem convallis condimentum.
+          </h1>
+          <br />
           <div>
-            <Button variant={'blue'} size={'rounded'}>
-              Episode 1
+            <Button>
+              Listen Now
+              <FaRegCirclePlay className="ml-4" />
             </Button>
           </div>
-          <div>Speaker Name | 33 mins</div>
-        </div>
-        <br />
-        <h1 className="text-5xl md:text-6xl font-medium tracking-wide">
-          Start career path as
-          <br /> a Product Designer <br />
-          in Startup
-        </h1>
-        <br />
-        <h1 className="text-4l text-gray-400">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est
-          leo. Nulla fermentum ultrices ullamcorper. Nam eleifend mi sed sem
-          convallis condimentum.
-        </h1>
-        <br />
-        <div>
-          <Button>
-            Listen Now
-            <FaRegCirclePlay className="ml-4" />
-          </Button>
         </div>
       </div>
-    </div>
+    </Containerf>
   )
 }
 
