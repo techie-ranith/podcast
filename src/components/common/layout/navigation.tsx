948cc34823b452198348c40baf04b2abf4242233
techie-ranith <ranith2k3@gmail.com>
2024-01-31 21:09:20 +0530

    fix: navigation bar

--- src/components/common/layout/navigation.tsx
+++ src/components/common/layout/navigation.tsx
@@ -1,8 +1,8 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { Button } from '../buttons'
-
+import Link from 'next/link'
 import logo from '/public/images/footer_icons/logo.png'
 import x from '/public/images/Vector.png'
 import logo2 from '/public/images/footer_icons/foss_colored_logo.png'
@@ -11,13 +11,33 @@ type Props = {}
 
 const Navigation = (props: Props) => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+      const isTop = scrollTop < 100
+      if (isTop !== isScrolled) {
+        setIsScrolled(isTop)
+      }
+    }
+
+    document.addEventListener('scroll', handleScroll)
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+    }
+  }, [isScrolled])
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen)
   }
 
   return (
-    <div className="flex items-center justify-between w-full pt-2">
+    <div
+      className={`flex items-center justify-between w-full pt-2 ${
+        isScrolled ? 'bg-white' : 'bg-transparent'
+      }`}
+    >
       <div className="flex items-center gap-8">
         <div>
           <Image src={logo} alt="images" />
@@ -57,29 +77,27 @@ const Navigation = (props: Props) => {
         </svg>
       </div>
 
-      {/* Mobile Menu */}
       <div
         className={`lg:hidden ${
           isMobileMenuOpen ? 'block' : 'hidden'
-        } absolute top-16 left-0 right-0 bg-white p-4 shadow-md text-center`} // Added 'text-center' class
+        } absolute top-16 left-0 right-0 bg-white p-4 shadow-md text-center`}
       >
         <div className="flex flex-col gap-4">
-          <div className="mb-2">Home</div>{' '}
-          {/* Added margin-bottom for spacing */}
-          <div className="mb-2">Episodes</div>{' '}
-          {/* Added margin-bottom for spacing */}
+          <div className="mb-2">Home</div> <div className="mb-2">Episodes</div>{' '}
           <div>About</div>
         </div>
-        {/* Removed Subscribe Button */}
       </div>
-
-      {/* Desktop Menu */}
       <div className="hidden lg:flex items-center justify-center gap-10">
-        <div>Home</div>
-        <div>Episodes</div>
-        <div>About</div>
+        <div>
+          <Link href="/">Home</Link>
+        </div>
+        <div>
+          <Link href="/episodes">Episodes</Link>
+        </div>
+        <div>
+          <Link href="/favorites">Favorites</Link>
+        </div>
       </div>
-
       <div className="hidden lg:flex items-center justify-end">
         <Button variant={'black-outline'} size={'default'}>
           Subscribe
